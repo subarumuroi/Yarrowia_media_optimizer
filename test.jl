584010dccb07ff564aa298025200bce9d18e427c
Subaru Muroi <uqkmuroi@uq.edu.au>
2025-07-17 17:12:31 +1000

    test julia on full run

--- test.jl
+++ test.jl
@@ -1,15 +1,16 @@
 using ProgrammaticPipetting, CSV, DataFrames, XLSX
 
-fname = "Yarrowia_gp_pretest_1.xlsx"
+#fname = "Yarrowia_gp_pretest_1.xlsx"
 #fname = path_example_xlsx()
+fname = "test-pipette-data-yarrowia1.xlsx"
 df_pipette = estimate_pipette_volumes(fname)
 
-names(df_pipette)
+#names(df_pipette)
 
-names(df_pipette)[2]
+#names(df_pipette)[2]
 
-sum(df_pipette[:, 2:end], dims = 2)
+#sum(df_pipette[:, 2:end], dims = 2)
 
 
-#CSV.write("testyarrow.csv", df_pipette)
+CSV.write("Yarrowia_experiment_1_stock_matrix.csv", df_pipette)
 
