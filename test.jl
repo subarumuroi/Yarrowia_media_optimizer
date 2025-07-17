8562c4b554f18ad43c02bcf7f5f73f273a497977
Subaru Muroi <uqkmuroi@uq.edu.au>
2025-07-17 13:36:51 +1000

    played around with julia code

--- test.jl
+++ test.jl
@@ -1,6 +1,15 @@
 using ProgrammaticPipetting, CSV, DataFrames, XLSX
 
-fname = "test-pipette-data-yarrowia1.xlsx"
-
+fname = "Yarrowia_gp_pretest_1.xlsx"
+#fname = path_example_xlsx()
 df_pipette = estimate_pipette_volumes(fname)
+
+names(df_pipette)
+
+names(df_pipette)[2]
+
+sum(df_pipette[:, 2:end], dims = 2)
+
+
+#CSV.write("testyarrow.csv", df_pipette)
 
